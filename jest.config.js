19b1b8e28ab392b4b0d3f4e3cc6ed9b59964a54d
Pho Huynh <pho.huynh@hellohealthgroup.com>
2020-12-17 17:17:48 +0700

    Update absolute import for jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ['src'],
+  roots: ['./src'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   testPathIgnorePatterns: ['node_modules/'],
@@ -12,6 +12,7 @@ module.exports = {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'identity-obj-proxy',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    'src/(.*)': '<rootDir>/$1',
   },
-  modulePaths: ['src/'],
+  modulePaths: ['./src'],
 }
